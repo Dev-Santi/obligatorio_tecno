7252edd5d40d7021f8085c59d29dce2d39954244
santinakc <santinakcfin@gmail.com>
2023-11-05 19:30:12 -0300

    santi push

---
+++ tests/actualizarNota.spec.js
@@ -0,0 +1,46 @@
+let { test, expect } = require("@playwright/test");
+
+let APIKey = "77c3e77d0160daa898cc6e67519d291f";
+let APIToken = "ATTAbc64b7578a2339528fc4c8614ce5c1529952f979e69bbfcbdb8f74598df30ad6CFA2A0BC";
+let urlBase = "https://api.trello.com";
+
+let idTablero = "gDSmvici"; // Identificador del tablero donde se realizan los cambios
+let idNota = "OVXHxZhS"; // Identificador de la nota que vamos a cambiar de lista.
+let listaReceptora = "Hecho"; // Nombre de la lista que recibe la nota.
+
+/* -------- */
+
+test("Mover nota a una nueva lista", async function ({ request }) {
+  let listas = await obtenerListas(request);
+  expect(listas.status()).toBe(200);
+
+  let listasParseadas = await listas.json();
+  let listaBuscada = obtenerUnaLista(listasParseadas);
+
+  let notaActualizada = await actualizarListaDeNota(request, listaBuscada);
+  expect(notaActualizada.status()).toBe(200);
+});
+
+/* -------- */
+
+async function obtenerListas(request) {
+  return await request.get(`${urlBase}/1/boards/${idTablero}/lists?key=${APIKey}&token=${APIToken}`);
+}
+
+function obtenerUnaLista(listas) {
+  let respuesta = "";
+  for (let i = 0; i < listas.length && !respuesta; i++) {
+    if (listas[i].name == listaReceptora) {
+      respuesta = listas[i].id;
+    }
+  }
+  return respuesta;
+}
+
+async function actualizarListaDeNota(request, lista) {
+  return await request.put(`${urlBase}/1/cards/${idNota}?key=${APIKey}&token=${APIToken}`, {
+    data: {
+      idList: lista,
+    },
+  });
+}
